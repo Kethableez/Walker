ccf3d6230777b0d31f7b170883a719740e478d5a
Kethableez <janiszyn.amadeusz@gmail.com>
2021-06-21 20:15:15 +0200

    Project initialized [FE + BE]. Configuration with MySQL database

---
+++ walker-api/src/main/java/com/kethableez/walkerapi/WalkerApiApplication.java
@@ -0,0 +1,13 @@
+package com.kethableez.walkerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkerApiApplication.class, args);
+	}
+
+}
---
+++ walker-api/src/test/java/com/kethableez/walkerapi/WalkerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kethableez.walkerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
